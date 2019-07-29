4fbdef52a4d9ec8fdd2f895bf4b5d3043fb7c181
kwasniew <kwasniewski.mateusz@gmail.com>
2019-07-29 16:35:45 +0200

    async await

--- src/app.js
+++ src/app.js
@@ -8,45 +8,36 @@ const url = 'mongodb://localhost:27017/booksapi';
 
 app.use(express.json());
 
-
-// let books;
-// MongoClient.connect(url, function(err, client) {
-    // setTimeout(() => {
-    //     books = client.db().collection("books");
-    // }, 10000);
-
-// });
 let booksPromise = MongoClient.connect(url).then(function(client) {
     return client.db().collection("books");
 });
 
-app.post("/book", function(req, res, next) {
+app.post("/book", async function (req, res, next) {
     const {title, authors, isbn, description} = req.body;
-
-    booksPromise.then(function(books) {
-        return books.updateOne(
+    try {
+        const books = await booksPromise;
+        await books.updateOne(
             {isbn: isbn},
             {$set : {title, authors, isbn, description} },
-            {upsert: true}
-        );
-    }).then(function() {
+            {upsert: true});
         res.json({title, authors, isbn, description});
-    }).catch(next);
-
+    } catch (e) {
+        next(e);
+    }
 });
 
-app.get("/book/:isbn", function (req, res, next) {
+app.get("/book/:isbn", async function (req, res, next) {
     const isbn = req.params.isbn;
-    booksPromise
-        .then(function (books) {
-            return books.findOne(
-                {isbn},
-                { projection: {_id: 0} }
-            );
-        })
-        .then(function (book) {
-            res.json(book);
-        }).catch(next);
+    try {
+        const books = await booksPromise;
+        const book = await books.findOne(
+            {isbn},
+            {projection: {_id: 0}}
+        );
+        res.json(book);
+    } catch(e) {
+        next(e);
+    }
 });
 
 app.use(function notFound(req, res, next) {
